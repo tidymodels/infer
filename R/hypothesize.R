02a76e6161208c3a60e80ea6749bf72870aac824
andrewpbray <andrew.bray@gmail.com>
2017-06-27 00:16:59 -0700

    add separate null and ... args for parameters values

--- R/hypothesize.R
+++ R/hypothesize.R
@@ -1,6 +1,6 @@
 #' Specify a null hypothesis
 #' @param x a data frame that can be coerced into a \code{\link[dplyr]{tbl_df}}
-#' @param null the null hypothesis
+#' @param null the null hypothesis. Options include "independence" and "point".
 #' @param ... arguments passed to downstream functions
 #' @importFrom dplyr as.tbl
 #' @export
@@ -30,7 +30,28 @@
 #'     tidy()
 #' }
 
-hypothesize <- function(x, null, ...) {
+hypothesize <- function(x, null = c("independence", "point"), ...) {
   attr(x, "null") <- null
+
+  # functions to check if null makes sense given classes of variables
+
+  dots <- list(...)
+  params <- parse_params(dots)
+  # functions to check if parameters make sense given classes of variables
+  attr(x, "params") <- params
+
   return(as.tbl(x))
+}
+
+parse_params <- function(x) {
+  # proportions
+  ## find props
+  prop_ind <- grep("p\\d+", names(x))
+  ## check that props are between 0 and 1 and sum to 1 if there are > 1
+
+  # means
+  ## find props
+  mean_ind <- grep("mu\\d+", names(x))
+
+  return(x[c(prop_ind, mean_ind)])
 }
