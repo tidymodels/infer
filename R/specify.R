f8448a827d92fc12448bb7c4ef69c1195856ac35
ismayc <chester.ismay@gmail.com>
2017-10-09 12:09:25 -0700

    Fix spacing and clarify specify() docs

--- R/specify.R
+++ R/specify.R
@@ -1,5 +1,5 @@
 #' Specify the response and explanatory variables
-#' @param x a data frame that can be coerced into a \code{\link[dplyr]{tbl_df}}
+#' @param x a data frame that can be coerced into a \code{\link[tibble]{tbl_df}}
 #' @param formula a formula with the response variable on the left and the explanatory on the right
 #' @param response the variable name in \code{x} that will serve as the response. This is alternative to using the \code{formula} argument.
 #' @param explanatory the variable name in \code{x} that will serve as the explanatory variable
@@ -25,7 +25,7 @@ specify <- function(x, formula, response = NULL, explanatory = NULL) {
   if (!all(
     as.character(
       c(attr(x, "response"),
-        attr(x,"explanatory")
+        attr(x, "explanatory")
         )
     ) %in% names(x)
   )) stop("The columns you specified could not be found.")
