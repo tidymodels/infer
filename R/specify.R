4a1b8a1e59a8f0de47a1b54bf908eecfdee1f9e5
ismayc <chester.ismay@gmail.com>
2017-10-09 17:53:33 -0700

    Included spot in specify to calculate degrees of freedom for t (Maybe not best spot?)

--- R/specify.R
+++ R/specify.R
@@ -60,6 +60,34 @@ specify <- function(x, formula, response = NULL, explanatory = NULL) {
       as.character(attr(x, "explanatory"))
     )))
 
+  # To help determine theoretical distribution to plot
+  if(is.null(attr(x, "response")))
+    attr(x, "response_type") <- NULL
+  else
+    attr(x, "response_type") <- class(x[[as.character(attr(x, "response"))]])
+  
+  if(is.null(attr(x, "explanatory")))
+    attr(x, "explanatory_type") <- NULL
+  else
+    attr(x, "explanatory_type") <- class(x[[as.character(attr(x, "explanatory"))]])
+  
+  # NOT SURE if this is the best place for this?
+  # Keep track of Satterthwaite degrees of freedom since lost when in aggregation w/
+  # calculate()/generate()
+  if (!is.null(attr(x, "response")) & 
+      !is.null(attr(x, "explanatory")) & 
+      !is.null(attr(x, "response_type")) & 
+      !is.null(attr(x, "explanatory_type"))){
+    if(attr(x, "response_type") %in% c("integer", "numeric") &
+       attr(x, "explanatory_type") == "factor"){
+      if(length(levels(x[[as.character(attr(x, "explanatory"))]])) == 2) {
+        attr(x, "distr_param") <- x %>% 
+          dplyr::summarize(stats::t.test(!! attr(x, "response") ~ !! attr(x, "explanatory"))[["parameter"]]) %>% 
+          dplyr::pull()
+      }
+    }
+  }
+  
   # add "infer" class
   class(x) <- append("infer", class(x))
   
