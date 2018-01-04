d81ef1cce55006e13b1eece09eb7cce752a452e9
ismayc <chester.ismay@gmail.com>
2018-01-04 12:25:00 -0800

    Remove tests on pipe since imported

--- tests/test-pipe.R
+++
@@ -1,7 +0,0 @@
-context("pipe")
-
-test_that("pipe works", {
-  piped <- iris %>% specify(Sepal.Length ~ Sepal.Width)
-  non_piped <- specify(iris, Sepal.Length ~ Sepal.Width)
-  expect_equal(pipe, non_piped)
-})
