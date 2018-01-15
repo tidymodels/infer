b859459afdcee264a87fc81a7ac81e615749e7c7
ismayc <chester.ismay@gmail.com>
2018-01-14 22:35:13 -0800

    Fix example

--- R/rep_sample_n.R
+++ R/rep_sample_n.R
@@ -19,8 +19,8 @@
 #' 
 #' @export
 #' @examples
-#' library(dplyr)
-#' library(ggplot2)
+#' suppressPackageStartupMessages(library(dplyr))
+#' suppressPackageStartupMessages(library(ggplot2))
 #' 
 #' # Create a virtual population of N = 2400 balls, of which 900 are red and the
 #' # rest are white
@@ -31,7 +31,7 @@
 #' )
 #' population
 #' 
-#' # Take samples of size n = 50 balls; do this 1000 times
+#' # Take samples of size n = 50 balls without replacement; do this 1000 times
 #' samples <- population %>%
 #'   rep_sample_n(size = 50, reps = 1000)
 #' samples
@@ -44,9 +44,9 @@
 #' 
 #' # Plot sampling distribution
 #' ggplot(p_hats, aes(x = prop_red)) + 
-#'   geom_histogram(binwidth = 0.05) + 
+#'   geom_density() + 
 #'   labs(x = "p_hat", y = "Number of samples", 
-#'   title = "Sampling distribution of p_hat based on 1000 samples of size n = 50")
+#'   title = "Sampling distribution of p_hat from 1000 samples of size 50")
 rep_sample_n <- function(tbl, size, replace = FALSE, reps = 1, prob = NULL) {
   n <- nrow(tbl)
   
