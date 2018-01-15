80a33f2eab292018fef34de7e06d634b29d6c995
ismayc <chester.ismay@gmail.com>
2018-01-14 21:47:52 -0800

    Add tests

--- tests/testthat/test-rep_sample_n.R
+++ tests/testthat/test-rep_sample_n.R
@@ -1,14 +1,22 @@
 context("rep_sample_n")
 
-N <- 2400
+N <- 5
 population <- tibble::data_frame(
   ball_ID = 1:N,
-  color = c(rep("red", 900), rep("white", N - 900))
+  color = factor(c(rep("red", 3), rep("white", N - 3)))
 )
 
-# calculate arguments
-test_that("x is a tibble", {
+test_that("rep_sample_n works", {
   expect_silent(population %>%
-                  rep_sample_n(size = 50, reps = 10))
+                  rep_sample_n(size = 2, reps = 10))
+  expect_error(population %>% 
+                  rep_sample_n(size = 2, reps = 10, 
+                               prob = rep(x = 1/5, times = 100)))
+  expect_error(population %>% 
+                 rep_sample_n(size = 2, reps = 10,
+                              prob = c(1/2, 1/2)))
+  expect_error(population %>% 
+                 rep_sample_n(size = 2, reps = 10,
+                              prob = c(1/5, 1/5, 1/5, 1/5, 1/5)))
 })
 
