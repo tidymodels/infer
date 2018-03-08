7de99a1f48a7cade60f17abf78161c88559617cc
ismayc <chester.ismay@gmail.com>
2018-03-07 16:04:22 -0800

    Update generate

--- R/generate.R
+++ R/generate.R
@@ -1,39 +1,40 @@
-#' Generate resamples of a type provided via \code{type} parameter
+#' Generate resamples, permutations, or simulations based on
+#' `specify` and (if needed) `hypothesize` inputs
 #' @param x a data frame that can be coerced into a \code{\link[dplyr]{tbl_df}}
 #' @param reps the number of resamples to generate
 #' @param type currently either \code{bootstrap}, \code{permute}, or \code{simulate}
 #' @param ... currently ignored
+#' @return A tibble containing \code{rep} generated datasets, indicated by the
+#' \code{replicate} column.
 #' @importFrom dplyr group_by
 #' @export
 #' @examples
-#' # Generate 100 bootstrap means
-#' # resulting in 100 * 32 = 3200 rows
-#' mtcars %>%
-#'    specify(response = mpg) %>%
-#'    generate(reps = 100, type = "bootstrap")
-#'    
-#' # Generate 200 permutations of two proportions example
-#' # resulting in 200 * 32 = 6400 rows
-#' if(require(dplyr)) {
-#'     mtcars %>%
-#'         mutate(am = factor(am), vs = factor(vs)) %>%
-#'         specify(am ~ vs) %>% # alt: response = am, explanatory = vs
-#'         hypothesize(null = "independence") %>%
-#'         generate(reps = 200, type = "permute")
-#'  }
-#'  
-#' # Generate 150 simulations assuming true probability
-#' # of success (a "1") is 0.25,
-#' # resulting in 150 * 32 = 4800 rows
-#' if(require(dplyr)) {
-#'     mtcars %>%
-#'         mutate(am = factor(am)) %>%
-#'         specify(response = am) %>% # alt: am ~ NULL (or am ~ 1)
-#'         hypothesize(null = "point", p = c("1" = .25)) %>% 
-#'         generate(reps = 150, type = "simulate") 
-#' }
+#' # Permutation test for two binary variables
+#'   mtcars %>%
+#'     dplyr::mutate(am = factor(am), vs = factor(vs)) %>%
+#'     specify(am ~ vs, success = "1") %>%
+#'     hypothesize(null = "independence") %>%
+#'     generate(reps = 100, type = "permute")
 
 generate <- function(x, reps = 1, type = "bootstrap", ...) {
+
+  if (type == "permute" &&
+      any(is.null(attr(x, "response")), is.null(attr(x, "explanatory")))) {
+    stop(paste("Please `specify()` an explanatory and a response variable",
+         "when permuting."))
+  }
+  if (type == "simulate" &&
+      attr(x, "null") != "point" &&
+      !(length(grep("p", names(attr(x, "params")))) >= 1)) {
+    stop("Simulation requires a `point` null hypothesis on proportions.")
+  }
+  if (type == "bootstrap" &&
+      !(attr(attr(x, "params"), "names") %in% c("mu", "med", "sigma")) &&
+      !is.null(attr(x, "null"))) {
+    stop(paste("Bootstrapping is inappropriate in this setting.",
+          "Consider using `type = permute` or `type = simulate`."))
+  }
+
   if (type == "bootstrap") {
     return(bootstrap(x, reps, ...))
   }
@@ -43,7 +44,9 @@ generate <- function(x, reps = 1, type = "bootstrap", ...) {
   if (type == "simulate") {
     return(simulate(x, reps, ...))
   }
-  x
+  if (!(type %in% c("bootstrap", "permute", "simulate")))
+    stop(paste("Choose one of the available options for `type`:",
+               '`"bootstrap"`, `"permute"`, or `"simulate"`'))
 }
 
 bootstrap <- function(x, reps = 1, ...) {
@@ -55,27 +58,30 @@ bootstrap <- function(x, reps = 1, ...) {
       col <- as.character(attr(x, "response"))
       x[[col]] <- x[[col]] - mean(x[[col]], na.rm = TRUE) + attr(x, "params")
     }
-    
-    # Similarly for median
-    if(attr(attr(x, "params"), "names") == "Med"){
-      col <- as.character(attr(x, "response"))
-      x[[col]] <- x[[col]] - stats::median(x[[col]], na.rm = TRUE) + attr(x, "params")
-    }
 
-    # Similarly for sd
-    if(attr(attr(x, "params"), "names") == "sd"){
+    # Similarly for median
+    if(attr(attr(x, "params"), "names") == "med"){
       col <- as.character(attr(x, "response"))
-      x[[col]] <- x[[col]] - stats::sd(x[[col]], na.rm = TRUE) + attr(x, "params")
+      x[[col]] <- x[[col]] - 
+        stats::median(x[[col]], na.rm = TRUE) + attr(x, "params")
     }
 
     # TODO: Similarly for t
 
     # TODO: Similarly for z  
+
+    # Similarly for sd
+    if(attr(attr(x, "params"), "names") == "sigma"){
+      col <- as.character(attr(x, "response"))
+      x[[col]] <- x[[col]] - 
+        stats::sd(x[[col]], na.rm = TRUE) + attr(x, "params")
+    }
   }
-  
+
   # Set variables for use in calculate()
   result <- rep_sample_n(x, size = nrow(x), replace = TRUE, reps = reps)
   attr(result, "response") <- attr(x, "response")
+  attr(result, "success") <- attr(x, "success")
   attr(result, "explanatory") <- attr(x, "explanatory")
   attr(result, "response_type") <- attr(x, "response_type")
   attr(result, "explanatory_type") <- attr(x, "explanatory_type")
@@ -100,6 +106,7 @@ permute <- function(x, reps = 1, ...) {
   
   attr(df_out, "null") <- attr(x, "null")
   attr(df_out, "response") <- attr(x, "response")
+  attr(df_out, "success") <- attr(x, "success")
   attr(df_out, "explanatory") <- attr(x, "explanatory")
   attr(df_out, "response_type") <- attr(x, "response_type")
   attr(df_out, "explanatory_type") <- attr(x, "explanatory_type")
@@ -120,7 +127,7 @@ permute_once <- function(x, ...) {
   if (attr(x, "null") == "independence") {
     y <- pull(x, !! attr(x, "response"))
 
-    y_prime <- sample(y, size = length(y), replace = TRUE)
+    y_prime <- sample(y, size = length(y), replace = FALSE)
     x[as.character(attr(x, "response"))] <- y_prime
     return(x)
   }
@@ -132,7 +139,7 @@ permute_once <- function(x, ...) {
 #' @importFrom rlang :=
 
 simulate <- function(x, reps = 1, ...) {
-  fct_levels <- levels(dplyr::pull(x, !! attr(x, "response")))
+  fct_levels <- as.character(unique(dplyr::pull(x, !! attr(x, "response"))))
 
   col_simmed <- unlist(replicate(reps, sample(fct_levels,
                                               size = nrow(x),
@@ -146,6 +153,7 @@ simulate <- function(x, reps = 1, ...) {
   attr(rep_tbl, "null") <- attr(x, "null")
   attr(rep_tbl, "params") <- attr(x, "params")
   attr(rep_tbl, "response") <- attr(x, "response")
+  attr(rep_tbl, "success") <- attr(x, "success")
   attr(rep_tbl, "explanatory") <- attr(x, "explanatory")
   attr(rep_tbl, "response_type") <- attr(x, "response_type")
   attr(rep_tbl, "explanatory_type") <- attr(x, "explanatory_type")
@@ -163,6 +171,7 @@ simulate <- function(x, reps = 1, ...) {
   
   ## From Chester: Upon further inspection, I think we'll need a bunch of these to 
   ## appropriately determine the theoretical distributions when they exist
+
   return(dplyr::group_by(rep_tbl, replicate))
 }
 
@@ -170,39 +179,11 @@ simulate <- function(x, reps = 1, ...) {
 
 format_params <- function(x) {
   par_levels <- get_par_levels(x)
-  fct_levels <- levels(dplyr::pull(x, !! attr(x, "response")))
+  fct_levels <- as.character(unique(dplyr::pull(x, !! attr(x, "response"))))
   return(attr(x, "params")[match(fct_levels, par_levels)])
 }
 
 get_par_levels <- function(x) {
   par_names <- names(attr(x, "params"))
   return(gsub("^.\\.", "", par_names))
-}
-
-#' @importFrom dplyr as_tibble pull data_frame inner_join
-
-# Modified oilabs::rep_sample_n() with attr added
-rep_sample_n <- function(tbl, size, replace = FALSE, reps = 1, prob = NULL) {
-  n <- nrow(tbl)
-
-  # assign non-uniform probabilities
-  # there should be a better way!!
-  # prob needs to be nrow(tbl) -- not just number of factor levels
-  if (!is.null(prob)) {
-    if (length(prob) != n) stop("The argument prob must have length nrow(tbl).")
-    df_lkup <- dplyr::data_frame(vals = levels(dplyr::pull(tbl, 1)))
-    names(df_lkup) <- names(tbl)
-    df_lkup$probs = prob
-    tbl_wgt <- dplyr::inner_join(tbl, df_lkup)
-    prob <- tbl_wgt$probs
-  }
-
-  i <- unlist(replicate(reps, sample.int(n, size, replace = replace, prob = prob),
-                        simplify = FALSE))
-  rep_tbl <- cbind(replicate = rep(1:reps, rep(size, reps)),
-                   tbl[i, ])
-  rep_tbl <- dplyr::as_tibble(rep_tbl)
-  names(rep_tbl)[-1] <- names(tbl)
-  #  attr(rep_tbl, "ci") <- attr(tbl, "ci")
-  dplyr::group_by(rep_tbl, replicate)
 }
