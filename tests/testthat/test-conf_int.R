83451d0caaa54eeb7f582d5889a6ec1597cd805e
ismayc <chester.ismay@gmail.com>
2018-06-14 17:42:39 -0700

    Update tests and pkgdown

--- tests/testthat/test-conf_int.R
+++ tests/testthat/test-conf_int.R
@@ -13,6 +13,11 @@ iris_calc <- iris_tbl %>%
   generate(reps = 1000) %>% 
   calculate(stat = "diff in props", order = c("large", "small"))
 
+obs_diff <-  iris_tbl %>%
+  specify(Sepal.Length.Group ~ Sepal.Width.Group,
+          success = "<=5") %>% 
+  calculate(stat = "diff in props", order = c("large", "small"))
+
 set.seed(2018)
 test_df <- tibble::tibble(stat = rnorm(100))
 
@@ -35,6 +40,9 @@ test_that("basics work", {
   )
   expect_silent(
     iris_calc %>% get_ci(type = "se", point_estimate = 4)
+  )
+  expect_silent(
+    iris_calc %>% get_ci(type = "se", point_estimate = obs_diff)
   )
   expect_error(
     iris_calc %>% get_ci(type = "se", point_estimate = "error")
