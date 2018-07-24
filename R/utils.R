cc3da195bce13338ce79159cb8d82480699a05f2
evgeni.chasnovski <evgeni.chasnovski@gmail.com>
2018-07-24 21:08:49 +0300

    Implement `glue_null()`: wrapper for `glue::glue()` that allows to print 'NULL' for code which evaluates in `NULL`.

--- R/utils.R
+++ R/utils.R
@@ -52,7 +52,7 @@ has_response <- function(x){
 stop_glue <- function(..., .sep = "", .envir = parent.frame(),
                       call. = FALSE, .domain = NULL) {
   stop(
-    glue::glue(..., .sep = .sep, .envir = .envir),
+    glue_null(..., .sep = .sep, .envir = .envir),
     call. = call., domain = .domain
   )
 }
@@ -60,7 +60,7 @@ stop_glue <- function(..., .sep = "", .envir = parent.frame(),
 warning_glue <- function(..., .sep = "", .envir = parent.frame(),
                          call. = FALSE, .domain = NULL) {
   warning(
-    glue::glue(..., .sep = .sep, .envir = .envir),
+    glue_null(..., .sep = .sep, .envir = .envir),
     call. = call., domain = .domain
   )
 }
@@ -68,9 +68,25 @@ warning_glue <- function(..., .sep = "", .envir = parent.frame(),
 message_glue <- function(..., .sep = "", .envir = parent.frame(),
                          .domain = NULL, .appendLF = TRUE) {
   message(
-    glue::glue(..., .sep = .sep, .envir = .envir),
+    glue_null(..., .sep = .sep, .envir = .envir),
     domain = .domain, appendLF = .appendLF
   )
+}
+
+glue_null <- function(..., .sep = "", .envir = parent.frame()) {
+  glue::glue(
+    ..., .sep = .sep, .envir = .envir, .transformer = null_transformer
+  )
+}
+
+# This allows to print 'NULL' in `glue()` for code which evaluates in `NULL`
+null_transformer <- function(text, envir) {
+  out <- eval(parse(text = text, keep.source = FALSE), envir)
+  if (is.null(out)) {
+    return("NULL")
+  }
+  
+  out
 }
 
 check_order <- function(x, explanatory_variable, order){
