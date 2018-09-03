a14987fa2b8893dd618dec243ded4f67309cd259
Richard Cotton <richierocks@gmail.com>
2018-09-03 08:40:26 -0400

    use get_p_value() & get_confidence_interval() consistently

--- R/conf_int.R
+++ R/conf_int.R
@@ -1,7 +1,6 @@
 #' Compute confidence interval
 #'
 #' Only simulation-based methods are (currently only) supported.
-#' `get_confidence_interval()` and `get_ci()` are both aliases of `conf_int()`.
 #'
 #' @param x Data frame of calculated statistics or containing attributes of
 #'   theoretical distribution values. Currently, dependent on statistics being
@@ -16,34 +15,42 @@
 #'
 #' @return A 1 x 2 tibble with values corresponding to lower and upper values in
 #'   the confidence interval.
+#' @section Aliases:
+#' `conf_int()` and `get_ci()` are deprecated aliases of `get_confidence_interval()`.
 #'
 #' @examples
+#' # Prepare the dataset
 #' mtcars_df <- mtcars %>%
 #'   dplyr::mutate(am = factor(am))
+#'
+#' # Calculate the difference in means in the dataset
 #' d_hat <- mtcars_df %>%
 #'   specify(mpg ~ am) %>%
 #'   calculate(stat = "diff in means", order = c("1", "0"))
+#'
+#' # Same calculation on 100 bootstrap replicates
 #' bootstrap_distn <- mtcars_df %>%
 #'   specify(mpg ~ am) %>%
-#'   generate(reps = 100) %>%
+#'   generate(reps = 100, type = "bootstrap") %>%
 #'   calculate(stat = "diff in means", order = c("1", "0"))
-#' bootstrap_distn %>% conf_int(level = 0.9)
-#' bootstrap_distn %>% conf_int(type = "se", point_estimate = d_hat)
 #'
-#' @name get_ci
-NULL
-
-#' @rdname get_ci
+#' # Use level to set the confidence level
+#' bootstrap_distn %>%
+#'   get_confidence_interval(level = 0.9)
+#'
+#' # To calculate std error, set the type and point estimate
+#' bootstrap_distn %>%
+#'   get_confidence_interval(type = "se", point_estimate = d_hat)
+#' @name get_confidence_interval
 #' @export
-conf_int <- function(x, level = 0.95, type = "percentile",
-                     point_estimate = NULL) {
+get_confidence_interval <- function(x, level = 0.95, type = "percentile",
+  point_estimate = NULL){
+
   check_ci_args(x, level, type, point_estimate)
 
-  if (type == "percentile") {
-    ci_vec <- stats::quantile(
-      x[["stat"]],
-      probs = c((1 - level) / 2, level + (1 - level) / 2)
-    )
+  if(type == "percentile") {
+    ci_vec <- stats::quantile(x[["stat"]],
+      probs = c((1 - level) / 2, level + (1 - level) / 2))
 
     ci <- tibble::tibble(ci_vec[1], ci_vec[2])
     names(ci) <- names(ci_vec)
@@ -52,47 +59,35 @@ conf_int <- function(x, level = 0.95, type = "percentile",
     multiplier <- stats::qnorm(1 - (1 - level) / 2)
     ci <- tibble::tibble(
       lower = point_estimate - multiplier * stats::sd(x[["stat"]]),
-      upper = point_estimate + multiplier * stats::sd(x[["stat"]])
-    )
+      upper = point_estimate + multiplier * stats::sd(x[["stat"]]))
   }
 
-  ci
+  return(ci)
 }
 
-check_ci_args <- function(x, level, type, point_estimate) {
-  if (!is.null(point_estimate)) {
-    if (!is.data.frame(point_estimate)) {
+check_ci_args <- function(x, level, type, point_estimate){
+
+  if(!is.null(point_estimate)){
+    if(!is.data.frame(point_estimate))
       check_type(point_estimate, is.numeric)
-    } else {
+    else
       check_type(point_estimate, is.data.frame)
-    }
   }
   check_type(x, is.data.frame)
   check_type(level, is.numeric)
-  if ((level <= 0) || (level >= 1)) {
+  if(level <= 0 || level >= 1){
     stop_glue("The value of `level` must be between 0 and 1 non-inclusive.")
   }
 
-  if (!(type %in% c("percentile", "se"))) {
+  if(!(type %in% c("percentile", "se"))){
     stop_glue('The options for `type` are "percentile" or "se".')
   }
 
-  if ((type == "se") && is.null(point_estimate)) {
-    stop_glue(
-      'A numeric value needs to be given for `point_estimate` for `type = "se"'
-    )
-  }
+  if(type == "se" && is.null(point_estimate))
+    stop_glue('A numeric value needs to be given for `point_estimate` ',
+      'for `type = "se"')
 
-  if ((type == "se") && is.vector(point_estimate)) {
+  if(type == "se" && is.vector(point_estimate))
     check_type(point_estimate, is.numeric)
-  }
 }
 
-
-#' @rdname get_ci
-#' @export
-get_ci <- conf_int
-
-#' @rdname get_ci
-#' @export
-get_confidence_interval <- conf_int
