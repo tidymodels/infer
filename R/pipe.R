7ec907aab41c1daba57ec6de12722de5c619c812
Simon P. Couch <simonpatrickcouch@gmail.com>
2020-06-13 11:15:45 -0700

    fix escaping, link to file rather than topic

--- R/pipe.R
+++ R/pipe.R
@@ -1,8 +1,8 @@
 #' Pipe
 #'
-#' Like \\{dplyr\\}, \\{infer\\} also uses the pipe function,
-#' \code{\link[magrittr]{\%>\%}} to turn function composition into a series of
-#' imperative statements.
+#' Like \{dplyr\}, \{infer\} also uses the pipe (\code{`%>%`}) function 
+#' from \code{magrittr} to turn function composition into a series of
+#' iterative statements.
 #'
 #' @param lhs,rhs Inference functions and the initial data frame.
 #'
